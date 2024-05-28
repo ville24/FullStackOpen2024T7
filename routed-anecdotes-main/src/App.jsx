f1800212b017f982daa093bfc4a575cefb40737b
ville24 <73233385+ville24@users.noreply.github.com>
2024-05-28 14:00:42 +0000

    T7.6

--- routed-anecdotes-main/src/App.jsx
+++ routed-anecdotes-main/src/App.jsx
@@ -3,6 +3,7 @@ import {
   Routes, Route, Link, useParams, Navigate
 } from 'react-router-dom'
 import { useState } from 'react'
+import  { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -62,39 +63,45 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
-
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
+  }
+
+  const handleReset = () => {
+    content.onReset()
+    author.onReset()
+    info.onReset()
   }
 
   return (
     <div>
       <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
+      {/*<form>*/}
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content}/>
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author}/>
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info}/>
         </div>
-        <button>create</button>
-      </form>
+        <button onClick={handleSubmit}>create</button>
+        <button onClick={handleReset}>reset</button>
+      {/*</form>*/}
     </div>
   )
 
