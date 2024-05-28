82dcdcde3b49d72a23e85913f9933649f7102081
ville24 <73233385+ville24@users.noreply.github.com>
2024-05-28 22:34:15 +0300

    Update App.jsx

--- routed-anecdotes-main/src/App.jsx
+++ routed-anecdotes-main/src/App.jsx
@@ -11,9 +11,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='/' style={padding}>anecdotes</a>
-      <a href='/create' style={padding}>create new</a>
-      <a href='/about' style={padding}>about</a>
+      <Link style={padding} to="/">anecdotes</Link>
+      <Link style={padding} to="/create">create new</Link>
+      <Link style={padding} to="/about">about</Link>
     </div>
   )
 }
@@ -23,7 +23,7 @@ const AnecdoteList = ({ anecdotes }) => {
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} ><a href={`/anecdotes/${anecdote.id}`}>{anecdote.content}</a></li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id} ><Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link></li>)}
     </ul>
   </div>
 )}
